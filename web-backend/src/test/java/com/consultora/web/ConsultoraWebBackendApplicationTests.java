99905de30e6c1c627407be869d16491ecf88f623
Matias <157364130+Cofre-dev@users.noreply.github.com>
2025-09-10 09:40:58 -0300

    add files from sping Initializr
    making the migration from django to spring boot

---
+++ web-backend/src/test/java/com/consultora/web/ConsultoraWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultora.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultoraWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
